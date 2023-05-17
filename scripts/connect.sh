7c270df7e0a22a861d1ab178ff13cb96fb4819bf
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-05-17 16:59:39 +0100

    feat: add bastion scripts (#21)

---
+++ scripts/connect.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+variables() {
+    if [ -z "$ARM_SUBSCRIPTION_ID" ]; then
+        echo "Variable ARM_SUBSCRIPTION_ID is unset"
+        exit 1
+    fi
+    if [ -z "$ARM_RESOURCE_GROUP" ]; then
+        echo "Variable ARM_RESOURCE_GROUP is unset"
+        exit 1
+    fi
+    if [ -z "$ARM_BASTION_RESOURCE_GROUP" ]; then
+        echo "Variable ARM_BASTION_RESOURCE_GROUP is unset"
+        exit 1
+    fi
+    if [ -z "$ARM_BASTION_NAME" ]; then
+        echo "Variable ARM_BASTION_NAME is unset"
+        exit 1
+    fi
+}
+
+{
+    export $(cat azure.env | xargs)
+    variables
+    echo "=> Connecting to $1"
+    az network bastion ssh \
+        --name "$ARM_BASTION_NAME" \
+        --resource-group "$ARM_BASTION_RESOURCE_GROUP" \
+        --target-resource-id "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$ARM_RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/$1" \
+        --auth-type "ssh-key" \
+        --username "adminuser" \
+        --ssh-key ".ssh/id_rsa"
+}
