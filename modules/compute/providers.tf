78abd9b8908e0a2898759e39eafd89acb1e8d37c
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-07-07 10:58:59 +0100

    chore: update version constraint

--- modules/compute/providers.tf
+++ modules/compute/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = ">=3.5.1"
+      version = "~> 3.5.1"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.61.0"
+      version = "~> 3.61.0"
     }
   }
 }
